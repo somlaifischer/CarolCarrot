3e48dea7a87b1d55c67aff021f7ccb641ff944c2
adam sf <adam@prezi.com>
2015-12-10 18:17:55 +0100

    smooth data

--- arduino/CarolCarrot/CarolCarrot.ino
+++ arduino/CarolCarrot/CarolCarrot.ino
@@ -10,14 +10,18 @@ int bongos[] = {60, 64, 61};
 int cymbals[] = {52, 42, 37, 76};
 
 int frame = 0;
-int pinValues[] = {0, 0, 0, 0};
+int smoothNum = 6;
+int pin0Values[] = {0, 0, 0, 0, 0, 0};
+int pin1Values[] = {0, 0, 0, 0, 0, 0};
+int pin2Values[] = {0, 0, 0, 0, 0, 0};
+int pin3Values[] = {0, 0, 0, 0, 0, 0};
+int averagePins[] = {0, 0, 0, 0};
 boolean pinPlaying[] = {false, false, false, false};
 int pinPlayheads[] = {0, 0, 0, 0};
 int pinPlayedAt[] = {0, 0, 0, 0};
 
-int treshold = 800;
-
-boolean debug = true;
+int treshold = 700;
+boolean debug = false;
 
 void setup()
 {
@@ -31,43 +35,72 @@ void loop()
 {
   frame++;
 
-  pinValues[0] = analogRead(0);
-  pinValues[1] = analogRead(1);
-  pinValues[2] = analogRead(2);
-  pinValues[3] = analogRead(3);
-
-  if (false)
+  pin0Values[frame % smoothNum] = analogRead(0);
+  pin1Values[frame % smoothNum] = analogRead(3);
+  pin2Values[frame % smoothNum] = analogRead(2);
+  pin3Values[frame % smoothNum] = analogRead(1);
+  calcAverages();
+  if (debug)
   {
     switch (frame % 4)
     {
       case 0:
-        Serial.println(pinValues[0]);
+        Serial.println(averagePins[0]);
         break;
       case 1:
         Serial.print("    ");
-        Serial.println(pinValues[1]);
+        Serial.println(averagePins[1]);
         break;
       case 2:
         Serial.print("        ");
-        Serial.println(pinValues[2]);
+        Serial.println(averagePins[2]);
         break;
       case 3:
         Serial.print("            ");
-        Serial.println(pinValues[3]);
+        Serial.println(averagePins[3]);
         break;
     }
 
   }
 
-  playSequence(0, carolLow, 4, 1, 800, 20);
-  playSequence(1, carolHigh, 4, 1, 800, 20);
-  playSequence(2, bongos, 3, 2, 800, 500);
-  playSequence(3, cymbals, 4, 3, 800, 500);
+  playSequence(0, carolLow, 4, 1, treshold, 20);
+  playSequence(1, carolHigh, 4, 1, treshold, 20);
+  playSequence(2, bongos, 3, 2, treshold, 500);
+  playSequence(3, cymbals, 4, 3, treshold, 500);
 
   delay(50);
 }
 
+void calcAverages()
+{
+  int add = 0;
+  for (int i = 0; i < smoothNum; i++)
+  {
+    add += pin0Values[i];
+  }
+  averagePins[0] = add / smoothNum;
 
+  add = 0;
+  for (int i = 0; i < smoothNum; i++)
+  {
+    add += pin1Values[i];
+  }
+  averagePins[1] = add / smoothNum;
+
+  add = 0;
+  for (int i = 0; i < smoothNum; i++)
+  {
+    add += pin2Values[i];
+  }
+  averagePins[2] = add / smoothNum;
+
+  add = 0;
+  for (int i = 0; i < smoothNum; i++)
+  {
+    add += pin3Values[i];
+  }
+  averagePins[3] = add / smoothNum;
+}
 
 void playSequence(int pin, int sequence[], int seqLength, int channel, int sensorLimit, int timeLimit)
 {
@@ -76,14 +109,14 @@ void playSequence(int pin, int sequence[], int seqLength, int channel, int senso
     //Serial.print("               ");
     //Serial.println(frame - pinPlayedAt[pin]);
   }
-  if (!pinPlaying[pin] && pinValues[pin] < sensorLimit)
+  if (!pinPlaying[pin] && averagePins[pin] < sensorLimit)
   {
     if (frame - pinPlayedAt[pin] > timeLimit) pinPlayheads[pin] = 0; // reset to start if tapped long ago
     MIDI.sendNoteOn(sequence[pinPlayheads[pin] % seqLength], 127, channel);
     pinPlaying[pin] = true;
     pinPlayedAt[pin] = frame;
   }
-  else if (pinPlaying[pin] && pinValues[pin] > sensorLimit)
+  else if (pinPlaying[pin] && averagePins[pin] > sensorLimit)
   {
     MIDI.sendNoteOff(sequence[pinPlayheads[pin] % seqLength], 127, channel);
     pinPlaying[pin] = false;
@@ -95,6 +128,7 @@ void playSequence(int pin, int sequence[], int seqLength, int channel, int senso
 // drum part
 /*
  * http://www.voidaudio.net/percussion.html
+ * http://soundprogramming.net/file-formats/general-midi-drum-note-numbers/
 // tr8080
 c1 bass drum
 a1 808 Mid Tom 2
